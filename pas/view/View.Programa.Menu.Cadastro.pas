713c5ba361cf2092f9633fb4dd18753fcf52b042
Dieferson Romanoski <diefe-ki@outlook.com>
2020-11-22 19:32:37 -0300

    Doc: Tela base cadastros iniciais criada

--- pas/view/View.Programa.Menu.Cadastro.pas
+++ pas/view/View.Programa.Menu.Cadastro.pas
@@ -4,11 +4,24 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TFrmCadastro = class(TForm)
-    Panel1: TPanel;
+    PnlPrograma: TPanel;
+    PnlPacientes: TPanel;
+    Titulo: TLabel;
+    Image1: TImage;
+    PnlOrientaçoes: TPanel;
+    Label1: TLabel;
+    Image2: TImage;
+    PnlUsuarios: TPanel;
+    Label2: TLabel;
+    Image3: TImage;
+    PnlSintomas: TPanel;
+    Label3: TLabel;
+    Image4: TImage;
   private
     { Private declarations }
   public
